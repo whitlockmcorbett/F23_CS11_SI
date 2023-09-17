04aa69fb8e8f272f9f9973ba4f6094f4c20c56b8
Liam Gilligan <liamdgilligan@gmail.com>
2023-09-17 12:24:57 -0700

    Added Exercise 1, README, and solution program

---
+++ week_4/exercise1/solution.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	double number1;
+	cout << "Enter the first number: ";
+	cin >> number1;
+	
+	string op_symbol; // the operator e.g. + - / *
+	cout << "Which operation? (* / + -): ";
+	cin >> op_symbol;
+
+	double number2;
+	cout << "Enter the second number: ";
+	cin >> number2;
+
+	double result; // set result equal to your answer
+
+	//-------------------- YOUR CODE BELOW -------------------------
+
+	if (op_symbol == "+") {
+		result = number1 + number2;
+	} else if (op_symbol == "-") {
+		result = number1 - number2;
+	} else if (op_symbol == "*") {
+		result = number1 * number2;
+	} else if (op_symbol == "/") {
+		result = number1 / number2;
+	} else {
+		cout << "Invalid operator." << endl;
+		return 0;
+	}
+
+	//-------------------- YOUR CODE ABOVE -------------------------
+	
+
+	cout << "Result is: " << result << endl;
+
+	return 0;
+}
