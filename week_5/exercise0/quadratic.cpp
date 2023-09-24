587a863d51e3bd8a5143c406b42f4c0efe391286
Stephanie L'Heureux <stephanielh1111@gmail.com>
2023-09-24 11:02:58 -0700

    Finished exercise 1

---
+++ week_5/exercise0/quadratic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// What libraries do you need?
+using namespace std;
+
+int main() {
+  // Take inputs for A, B, and C
+
+  // Use the quadratic formula to calculate the roots 
+
+  // Output the result(s)
+  
+  // Uncomment when you are ready to output the results
+  // cout << "x = " << x1 << " or " << x2 << endl;
+  return 0;
+}
