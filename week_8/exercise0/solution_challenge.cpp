516483ec68d9b12dd55e76b56a7b417388efc256
Stephanie L'Heureux <stephanielh1111@gmail.com>
2023-10-08 19:45:21 -0700

    Changed comment wording

--- week_8/exercise0/solution_challenge.cpp
+++ week_8/exercise0/solution_challenge.cpp
@@ -9,8 +9,8 @@ int main() {
   getline(cin, str);
 
   for(int i = str.size() -1, j = 0; i >= str.size()/2 && j < str.size()/2 && pal; i--, j++) { 
-    while (!(str[i] >= 'A' && str[i] <= 'Z') && !(str[i] >= 'a' && str[i] <= 'z')) i--; // Increment while not a letter
-    while (!(str[j] >= 'A' && str[j] <= 'Z') && !(str[j] >= 'a' && str[j] <= 'z')) j++; // Increment while not a letter
+    while (!(str[i] >= 'A' && str[i] <= 'Z') && !(str[i] >= 'a' && str[i] <= 'z')) i--; // Skip while not a letter
+    while (!(str[j] >= 'A' && str[j] <= 'Z') && !(str[j] >= 'a' && str[j] <= 'z')) j++; // Skip while not a letter
     if (str[i] >= 'A' && str[i] <= 'Z') str[i] += 32; // if capital, make lower 
     if (str[j] >= 'A' && str[j] <= 'Z') str[j] += 32; // if capital, make lower
     pal = str[i] == str[j];
