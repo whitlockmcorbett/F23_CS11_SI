bf8528f1d788e169d4730d04353d71c379d070b5
Liam Gilligan <liamdgilligan@gmail.com>
2023-10-16 10:22:51 -0700

    forogt this guy lol

--- week_8/monday/exercise2/solution.cpp
+++ week_8/monday/exercise2/solution.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main() {
-  int a = 0, b = 8, n = 1000;
-  double delta_x = (b - a) / (double)n;
-  double result, x;
+  int n;
+  bool prime;
 
-  result = (sin(M_PI * a) + 2) + (sin(M_PI * b) + 2);
+  cout << "Enter an integer n: ";
+  cin >> n;
 
-  for (int i = 1; i < n; i++) {
-    x = a + i * delta_x;
-    if (i % 2 == 0) { // Subscript is even 
-      result += 2 * (sin(M_PI * x) + 2);
-    } else { // Subscript is odd
-      result += 4 * (sin(M_PI * x) + 2);
+  cout << 2 << " ";
+  for (int i = 3; i <= n; i+=2) {
+    prime = true;
+    for (int j = 2; j * j <= i; j++) {
+      if (i % j == 0) {
+        prime = false;
+        break;
+      }
+    }
+    if (prime) {
+      cout << i << " ";
     }
   }
-  result *= delta_x / 3.0;
-
-  cout << "The area below the function sin(πx)+2 between x = " << a << " and " 
-       << "x = " << b << " with " << n << " subdivisions is " << result << endl;
+  cout << endl;
 
   return 0;
 }
